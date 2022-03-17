7fe461e533a6867a2ec249717b513e8088a627c2
smulumbu <smulumbu@gmail.com>
2022-03-17 13:08:25 +0200

    Affichage quantity

--- affichage.js
+++ affichage.js
@@ -3,27 +3,38 @@ const message = 'TP SABIN MULUMBU' // Try edit me
 
 
 
-function affichage(nombre) {
+function affichage(marketPrice) {
 
 var precision =0;
 
-if (nombre < 0.0000001)
+if (marketPrice < 0.0000001)
 {precision = 10;}
-else if (0.0000001 < nombre && nombre < 0.00001)
+else if (0.0000001 < marketPrice && marketPrice < 0.00001)
   {precision = 9;}
-else if (0.00001 < nombre && nombre < 0.001)
+else if (0.00001 < marketPrice && marketPrice < 0.001)
   {precision = 7;}
-else if (0.001 < nombre && nombre < 0.1)
+else if (0.001 < marketPrice && marketPrice < 0.1)
  { precision = 6;}
-else if (0.1 < nombre && nombre < 1)
+else if (0.1 < marketPrice && marketPrice < 1)
   {precision = 5;}
-else if (1 < nombre && nombre < 10)
+else if (1 < marketPrice && marketPrice < 10)
 {precision = 3;}
-else if (10 < nombre)
+else if (10 < marketPrice)
   {precision = 2;}
-console.log(precision);
-return nombre.toFixed(precision);
+return marketPrice.toFixed(precision);
 }
 
-  var resultat = affichage(0.0000010000166);
-  console.log(resultat)
+function affichageCurrencies(marketPrice, quantity) {
+
+  var precision =0;
+
+  if (marketPrice < 1)
+  {precision = 2;}
+  else if (1 < marketPrice && marketPrice < 1000)
+    {precision = 3;}
+  else if (1000 < marketPrice && marketPrice < 10000)
+  {precision = 4;}
+  else if (10000 < marketPrice)
+    {precision = 6;}
+  return quantity.toFixed(precision);
+  }
